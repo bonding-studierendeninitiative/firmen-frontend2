796a9c8e6f1eeb502153628c4bd3060f6a005a05
mkrass-syntax <maximilian.krass@syntax-institut.de>
2024-08-16 14:26:31 +0200

    feat(event): add details schema

--- src/lib/schemas/eventDetailsSchema.ts
+++ src/lib/schemas/eventDetailsSchema.ts
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGE_KEYS } from '@constant';
 import { z } from 'zod';
+import * as v from 'valibot';
 
 const { required, greaterStartDate, greaterStartTime } = ERROR_MESSAGE_KEYS;
 
@@ -57,3 +58,27 @@ export const eventDetailSchema = z
 		}
 	);
 export type eventDetailTypes = z.infer<typeof eventDetailSchema>;
+
+export const EventDetailsResponseSchema = v.object({
+	id: v.string(),
+	name: v.string(),
+	location: v.optional(v.string()),
+	projectHSG: v.optional(v.string()),
+	dateFrom: v.nullable(v.pipe(v.string(), v.isoDate())),
+	dateTo: v.nullable(v.pipe(v.string(), v.isoDate())),
+	timeFrom: v.nullable(v.pipe(v.string(), v.isoTimeSecond())),
+	timeTo: v.nullable(v.pipe(v.string(), v.isoTimeSecond())),
+	comment: v.optional(v.string()),
+	latestRegistration: v.nullable(v.pipe(v.string(), v.isoDate())),
+	notesCategory: v.optional(v.string()),
+	descriptionNotes: v.optional(v.string()),
+	contactMail: v.optional(v.string()),
+	website: v.optional(v.string()),
+	additionalLink: v.optional(v.string()),
+	createdAt: v.pipe(v.string(), v.isoTimestamp()),
+	modifiedAt: v.nullable(v.pipe(v.string(), v.isoTimestamp())),
+	addons: v.array(v.object({})),
+	packages: v.array(v.object({}))
+});
+
+export type EventDetailsResponse = typeof EventDetailsResponseSchema;
