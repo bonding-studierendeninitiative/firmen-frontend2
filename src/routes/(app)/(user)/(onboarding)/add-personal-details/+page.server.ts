12ebedb6abe729ccf5575d3683b556940380da76
benedikt <benedikt-ricken@protonmail.com>
2025-10-07 14:16:31 +0200

    streamline onboarding workflow

--- src/routes/(app)/(user)/(onboarding)/add-personal-details/+page.server.ts
+++ src/routes/(app)/(user)/(onboarding)/add-personal-details/+page.server.ts
@@ -1,9 +1,3 @@
-import { superValidate } from 'sveltekit-superforms';
-import { valibot } from 'sveltekit-superforms/adapters';
-import { UpdateUserDetailsRequest } from '@schema';
-import { fail, redirect } from '@sveltejs/kit';
-import { updateMetadata } from '@/remote/functions';
-
 export const load = async ({ parent }) => {
 	// const { session } = await parent();
 	// if (!session?.id) return;
